d3e6a4d408a5ba2c7958e125048a4d1543e2ab10
Pratik Poojari <poojari.pr@northeastern.edu>
2023-04-18 18:56:45 -0400

    Update

--- FinalAPPLICATION/src/Business_Medicine/MedicineItem.java
+++ FinalAPPLICATION/src/Business_Medicine/MedicineItem.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business.MedicineItems;
+package Business_Medicine;
 
 /**
  *
- * @author Raunak Singh Matharu
+ * @author Pratik Poojari
  */
 public class MedicineItem {
     private int id;
